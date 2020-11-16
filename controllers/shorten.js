70d59d2b63fc3d4bfca0cb68c4bfd5845d8a6d10
pmayur <thisispatil@gmail.com>
2020-11-16 23:05:18 +0530

    Add functionality for shortening urls recieved

--- controllers/shorten.js
+++ controllers/shorten.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const helper = require('../util/helper')
+const shorten = express.Router();
 
-var shorten = express.Router();
+const ShortUrlModel = require('../models/ShortUrl')
 
-shorten.get('/', function(req, res) {
-    res.send("Hello from route")
+shorten.post('/', function(req, res) {
+    const url = req.body.url;
+    const isUrl = helper.checkIfUrl(url);
+    const newId = helper.makeId();
+
+    let newUrl = {
+        url: url,
+        short: newId
+    }
+
+    ShortUrlModel.ShortUrl.create(newUrl, function(err, result) {
+        if (err) {
+            res.send({
+                error: err.name,
+                errorCode: err.code
+            })
+        }    
+        else res.send({
+            url: result.url,
+            short: result.short
+        })
+    })
 })
 
 module.exports = shorten;
